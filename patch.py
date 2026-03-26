45b96cf89e7db206750390938207f499ddfb0310
Sllay <seu_email_aqui>
2026-03-26 05:47:59 -0300

    Fix: rewrite patch.py avoiding heredoc newline issues

--- patch.py
+++ patch.py
@@ -1,30 +1,35 @@
-import glob, os
+import glob
 
 files = glob.glob("/home/seluser/venv/**/anipy_server/main.py", recursive=True)
 if not files:
-    print("main.py nao encontrado!")
+    print("ERRO: main.py nao encontrado!")
     exit(1)
 
 path = files[0]
-print(f"Patchando: {path}")
+print("Patchando: " + path)
 
 with open(path, "r") as f:
-    code = f.read()
+    lines = f.readlines()
 
-old = 'options.add_argument("--no-sandbox")'
-new = ('options.add_argument("--no-sandbox")
+old = '        options.add_argument("--no-sandbox")
 '
-       '        options.add_argument("--dns-prefetch-disable")
+new = (
+    '        options.add_argument("--no-sandbox")
 '
-       '        options.binary_location = "/opt/chrome147/chrome"')
+    '        options.add_argument("--dns-prefetch-disable")
+'
+    '        options.binary_location = "/opt/chrome147/chrome"
+'
+)
 
-if old not in code:
-    print("Alvo nao encontrado no arquivo!")
+content = "".join(lines)
+if old not in content:
+    print("ERRO: alvo nao encontrado!")
     exit(1)
 
-code = code.replace(old, new, 1)
+content = content.replace(old, new, 1)
 
 with open(path, "w") as f:
-    f.write(code)
+    f.write(content)
 
 print("Patch aplicado com sucesso!")
